f94441727efd1fcbfd45d84aef986d1e8bae4a92
nevesfelipe47 <santosfelpe47@gmail.com>
2021-12-23 06:06:54 -0300

    exercicios cubo Prof Jeangrei Veiga

---
+++ Projeto Cubo  - Java P.O.O/ProjetoJava_FelipeNeves/ProjetoHelloWorld/src/br/com/aulajava/HelloWorld.java
@@ -0,0 +1,23 @@
+package br.com.aulajava;
+
+public class HelloWorld {
+
+	private static String mensagem ;
+	
+	/**
+	 * Metodo principal
+	 * @param args
+	 */
+		public static void main (String[]args) {
+			// atribuindo valor para variavel 
+			mensagem = "Olá mundo!!";
+			
+			// exibindo o valor atribuido
+			
+			System.out.println(mensagem);
+		}
+		
+
+	
+
+}
